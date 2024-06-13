bcfe0baf88515ec701439904439b744fafa6c705
rmellov <rmellov@gmail.com>
2024-06-12 21:37:33 -0300

    Continuando implementação das categorias

--- FinWebMvc/Controllers/CategoriaController.cs
+++ FinWebMvc/Controllers/CategoriaController.cs
@@ -19,39 +19,32 @@ namespace FinWebMvc.Controllers
             _context = context;
         }
 
-        // GET: Categoria
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Categorias.ToListAsync());
+            return View(await _context.Categorias
+                .OrderBy(x => x.Descricao)
+                .ToListAsync());
         }
 
-        // GET: Categoria/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
             var categoria = await _context.Categorias
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (categoria == null)
-            {
                 return NotFound();
-            }
 
             return View(categoria);
         }
 
-        // GET: Categoria/Create
         public IActionResult Create()
         {
             return View();
         }
-
-        // POST: Categoria/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+                
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Descricao,Tipo")] Categoria categoria)
@@ -62,36 +55,29 @@ namespace FinWebMvc.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
             return View(categoria);
         }
 
-        // GET: Categoria/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
             var categoria = await _context.Categorias.FindAsync(id);
+
             if (categoria == null)
-            {
                 return NotFound();
-            }
+
             return View(categoria);
         }
-
-        // POST: Categoria/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Descricao,Tipo")] Categoria categoria)
         {
             if (id != categoria.Id)
-            {
                 return NotFound();
-            }
 
             if (ModelState.IsValid)
             {
@@ -103,47 +89,39 @@ namespace FinWebMvc.Controllers
                 catch (DbUpdateConcurrencyException)
                 {
                     if (!CategoriaExists(categoria.Id))
-                    {
                         return NotFound();
-                    }
                     else
-                    {
                         throw;
-                    }
                 }
+
                 return RedirectToAction(nameof(Index));
             }
+
             return View(categoria);
         }
 
-        // GET: Categoria/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
             var categoria = await _context.Categorias
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (categoria == null)
-            {
                 return NotFound();
-            }
 
             return View(categoria);
         }
 
-        // POST: Categoria/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var categoria = await _context.Categorias.FindAsync(id);
+
             if (categoria != null)
-            {
                 _context.Categorias.Remove(categoria);
-            }
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
